5701e9274f93a706a89a8e379c04cbb0c8fd0ede
ebo2022 <99616031+ebo2022@users.noreply.github.com>
2024-07-30 20:15:32 -0400

    Use correct registry access in parameter list method

--- common/src/main/java/dev/ebo2022/matterhorn/common/world/level/levelgen/biome/MatterhornBiomeParameters.java
+++ common/src/main/java/dev/ebo2022/matterhorn/common/world/level/levelgen/biome/MatterhornBiomeParameters.java
@@ -1,12 +1,10 @@
 package dev.ebo2022.matterhorn.common.world.level.levelgen.biome;
 
 import com.mojang.datafixers.util.Pair;
-import dev.ebo2022.matterhorn.common.platform.PlatformUtil;
 import dev.ebo2022.matterhorn.core.Matterhorn;
 import net.minecraft.core.RegistryAccess;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.Climate;
 import net.minecraft.world.level.biome.MultiNoiseBiomeSource;
@@ -19,10 +17,7 @@ public class MatterhornBiomeParameters {
     private static final ResourceKey<LevelStem> PLACEHOLDER_KEY = ResourceKey.create(Registries.LEVEL_STEM, Matterhorn.id("placeholder"));
 
     public static List<Pair<Climate.ParameterPoint, ResourceKey<Biome>>> getList(RegistryAccess registryAccess) {
-        LevelStem levelStem = PlatformUtil.getCurrentServer()
-                .map(MinecraftServer::registryAccess)
-                .map(access -> access.registryOrThrow(Registries.LEVEL_STEM).getOrThrow(PLACEHOLDER_KEY))
-                .orElseThrow(() -> new IllegalStateException("No biome data found for Matterhorn"));
+        LevelStem levelStem = registryAccess.registryOrThrow(Registries.LEVEL_STEM).getOrThrow(PLACEHOLDER_KEY);
         if (levelStem.generator().getBiomeSource() instanceof MultiNoiseBiomeSource biomeSource) {
             return biomeSource.parameters().values().stream().map(pair -> pair.mapSecond(holder -> holder.unwrapKey().orElseThrow())).toList();
         } else {
